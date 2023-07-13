33ea5d9f6a78d0d58c66834e796b40aa54e4a66d
Danilo Micaías Lima da Silva <danilo.micaias.sga@gmail.com>
2023-07-12 22:12:00 -0300

    Add menu screen

---
+++ pirate-loot/Levels/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready():
+	$Controls/start.grab_focus()
+
+func _on_start_pressed():
+	get_tree().change_scene("res://Levels/Level_01.tscn")
+
+
+func _on_controls_pressed():
+	get_tree().change_scene("res://Scenes/controlsScreen.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
